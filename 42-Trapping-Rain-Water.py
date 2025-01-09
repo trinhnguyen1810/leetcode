8db0734f1dc99d937e1706acdc3cba8ad099fb9a
trinhnguyen1810 <105156592+trinhnguyen1810@users.noreply.github.com>
2025-01-09 09:58:03 -0800

    [42] [Time Beats: 61.84%] [Memory Beats: 28.91%] - LeetPush

---
+++ 42-Trapping-Rain-Water.py
@@ -0,0 +1,25 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        """
+        two pointers
+        If you swap left and right + -> maybe it would add negative
+        -> be aware
+        """
+        if not height:
+            return 0
+
+        l, r = 0, len(height) - 1
+        leftMax, rightMax = height[l], height[r]
+        res = 0
+
+        while l < r:
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                res += leftMax - height[l]
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                res += rightMax - height[r]
+
+        return res
